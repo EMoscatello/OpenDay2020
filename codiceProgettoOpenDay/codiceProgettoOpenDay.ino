7e00450a8234160bb7a83bc433263284b3912735
MarcoAnge02 <65389327+MarcoAnge02@users.noreply.github.com>
2020-12-05 10:49:15 +0100

    Add files via upload

--- codiceProgettoOpenDay/codiceProgettoOpenDay.ino
+++ codiceProgettoOpenDay/codiceProgettoOpenDay.ino
@@ -1,16 +1,22 @@
 #include "FastLED.h"
 
+//indico il numero di led
 #define NUM_LEDS 50 
 
+//indico il led centrale
 #define CENTER_LED 21
 
+//indico il pin 
 #define DATA_PIN 5 
 
+//indico il modello dei led
 #define LED_TYPE WS2811
 #define COLOR_ORDER RGB 
 
+//indico la luminosità
 #define BRIGHTNESS 50
 
+//definisco le difficoltà 
 #define EASY 1
 #define MEDIUM 2
 #define HARD 3
@@ -20,20 +26,28 @@
 #define LIGHT_SPEED 7
 #define MISSION_IMPOSSIBLE 8
 
+//dichiarazione variabile difficulty(difficoltà del livello)
 int difficulty = 1;
 
+//definiamo il numero di led
 CRGB leds[NUM_LEDS];
 
+//quando il giocatore vince questo tag è usato per il parametro della difficolta
 bool wonThisRound = false;
 
+//inizializzo la posizione per i led
 int LEDaddress = 0;
 
+//variabile booleana per vedere se si sta giocando
 bool Playing = true;
 
+//variabile booleana per verificare se è la prima vittoria
 bool CycleEnded = true;
-s
+
+//dichiarazioni per il pulsante
 const int buttonPin = 2;
 int buttonState = 0;
+
 
 void setup() 
 {
@@ -43,24 +57,37 @@ void setup()
   Serial.begin(9600);
 }
 
+
+//due modi - gioco e fine del gioco
 void loop() 
 {
+  //fine del gioco
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH)
   {
     Playing = false;
+    //il gicocatore ha schiacciato il bottone, e il led si è fermato sopra al led vincente
      for (int i = 0; i < NUM_LEDS; i++)
      {
-      leds [i] = CRGB::Black;
+      //imposto il led i al colore nero
+      leds[i] = CRGB::Black;
      }
+
+    //setto tutti i led di colore rosso
     leds[CENTER_LED] = CRGB::Red;
-    leds[LEDaddress] = CRGB::Green;  
+    //setto il led vincente di colore verde
+    leds[LEDaddress] = CRGB::Green;
+    //faccio accendere i led
     FastLED.show();
+    
     if (CycleEnded = true)
     {
+      //calcolo la distanza tra il led su cui mi sono fermato e il led vincente
       int diff = abs(CENTER_LED - LEDaddress);
+      //se la differenza è 0 allora ho vinto
       if (diff == 0)
       {
+        //ho vinto il livello
         wonThisRound = true;
         if (difficulty != MISSION_IMPOSSIBLE)
          {
@@ -93,21 +120,28 @@ void loop()
     LEDaddress = 0; 
     delay(250);
     buttonState = digitalRead(buttonPin);
+    //se il bottone non è stato premuto
     if (buttonState == LOW)
     {
      Playing = true;
     }
   }
   
+  //se sto giocando
   if(Playing)
   {
     for (int i = 0; i < NUM_LEDS; i++)
     {
+      //reimpostiamo il led i al colore nero
       leds[i] = CRGB::Black;
     }
+    //setto tutti i led di colore rosso
     leds[CENTER_LED] = CRGB::Red;
+    //setto il led vincente di colore verde
     leds[LEDaddress] = CRGB::Green;
+    //faccio accendere i led
     FastLED.show();
+    //faccio accendere un led alla volta
     LEDaddress++;
     if (LEDaddress == NUM_LEDS)
     {
@@ -123,7 +157,8 @@ void loop()
  }
 }
 
-int getTime(int diff)
+//parametri per i livelli
+int getTime(int diff) //ritorna il delay dei led in base alla difficoltà
 {
   int timeValue = 0;
   switch (diff) 
@@ -152,9 +187,11 @@ int getTime(int diff)
     case MISSION_IMPOSSIBLE:
       timeValue = 7;
   }
+  //ritorna il valore del delay
   return timeValue;
 }
 
+//a ogni vincita incremento i parametri della difficoltà
 void increaseDifficulty() 
 {
   if (difficulty != MISSION_IMPOSSIBLE && wonThisRound) 
@@ -163,6 +200,7 @@ void increaseDifficulty()
   }
 }
 
+//accensione dei led alla perdita
 void flash()
 { 
   fill_solid(leds, NUM_LEDS, CRGB::Red);
@@ -173,34 +211,47 @@ void flash()
   delay(500);
 }
 
-void fadeall() 
-{ 
-  for(int i = 0; i < NUM_LEDS; i++) 
+//accensione dei led alla vincita
+void fadeall()
+{
+  for(int i = 0; i < NUM_LEDS; i++)
   {
     leds[i].nscale8(250); 
-  } 
+  }
 }
 
+//accensione dei led in caso di vittoria
 void cylon() 
 { 
   static uint8_t hue = 0;
   Serial.print("x");
-  for(int i = 0; i < NUM_LEDS; i++) 
-  {
+  //prima slido i led in una direzione
+  for(int i = 0; i < NUM_LEDS; i++) {
+    //setto il led i rosso 
     leds[i] = CHSV(hue++, 255, 255);
+    //faccio accendere i led
     FastLED.show(); 
+    //ora che li abbiamo accesi, li reimpostiamo al colore nero
     //leds[i] = CRGB::Black;
+    //richiamo il metodo per accendere i led alla vincita
     fadeall();
+    //aspettiamo un piccolo tempo prima di ripetere il metodo
     delay(10);
   }
   Serial.print("x");
 
+  //adesso andiamo nell'altra direzione  
   for(int i = (NUM_LEDS)-1; i >= 0; i--) 
   {
+    //setto il led i rosso 
     leds[i] = CHSV(hue++, 255, 255);
+    //faccio accendere i led
     FastLED.show();
+    //ora che li abbiamo accesi, li reimpostiamo al colore nero
     //leds[i] = CRGB::Black;
+    //richiamo il metodo per accendere i led alla vincita
     fadeall();
+    //aspettiamo un piccolo tempo prima di ripetere il metodo
     delay(10);
   }
 }
