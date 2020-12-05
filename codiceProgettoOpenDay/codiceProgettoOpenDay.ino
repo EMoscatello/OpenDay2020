a43cde681e76283780710ae419e764e5ccc20a91
EMoscatello <65389302+EMoscatello@users.noreply.github.com>
2020-12-05 08:45:09 +0100

    Add files via upload

--- codiceProgettoOpenDay/codiceProgettoOpenDay.ino
+++ codiceProgettoOpenDay/codiceProgettoOpenDay.ino
@@ -1,52 +1,206 @@
-#include <FastLED.h>
+#include "FastLED.h"
+
 #define NUM_LEDS 50 
 
-#define DATA_PIN 5
-#define CLOCK_PIN 13
+#define CENTER_LED 21
 
+#define DATA_PIN 5 
 
+#define LED_TYPE WS2811
+#define COLOR_ORDER RGB 
+
+#define BRIGHTNESS 50
+
+#define EASY 1
+#define MEDIUM 2
+#define HARD 3
+#define ON_SPEED 4
+#define SONIC_SPEED 5
+#define ROCKET_SPEED 6
+#define LIGHT_SPEED 7
+#define MISSION_IMPOSSIBLE 8
+
+int difficulty = 1;
 
 CRGB leds[NUM_LEDS];
-int num=0;
 
-void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(57600);
-  Serial.println("resetting");
-  LEDS.addLeds<WS2811,DATA_PIN,RGB>(leds,NUM_LEDS);
-  LEDS.setBrightness(84);
+bool wonThisRound = false;
 
-  attachInterrupt(0,interruptBottone,RISING);
+int LEDaddress = 0;
+
+bool Playing = true;
+
+bool CycleEnded = true;
+s
+const int buttonPin = 2;
+int buttonState = 0;
+
+void setup() 
+{
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
+void loop() 
+{
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH)
+  {
+    Playing = false;
+     for (int i = 0; i < NUM_LEDS; i++)
+     {
+      leds [i] = CRGB::Black;
+     }
+    leds[CENTER_LED] = CRGB::Red;
+    leds[LEDaddress] = CRGB::Green;  
+    FastLED.show();
+    if (CycleEnded = true)
+    {
+      int diff = abs(CENTER_LED - LEDaddress);
+      if (diff == 0)
+      {
+        wonThisRound = true;
+        if (difficulty != MISSION_IMPOSSIBLE)
+         {
+          for (int i = 0; i < 2; i++)
+           {
+            cylon();
+           }
+         }
+        if (difficulty == MISSION_IMPOSSIBLE)
+         {
+          for (int i = 0; i < 8; i++)
+           {
+            cylon();
+           }
+          difficulty = 0; 
+         }
+        increaseDifficulty();      
+        wonThisRound = false;
+      }
+       else
+      {
+      delay(1000);  
+      for (int i = 0; i < 2; i++)
+       {
+        flash();
+       }
+      }
+        CycleEnded = false;
+      }
+    LEDaddress = 0; 
+    delay(250);
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == LOW)
+    {
+     Playing = true;
+    }
+  }
   
-  // put your main code here, to run repeatedly:
-  leds[25]=CRGB::Green;
+  if(Playing)
+  {
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+      leds[i] = CRGB::Black;
+    }
+    leds[CENTER_LED] = CRGB::Red;
+    leds[LEDaddress] = CRGB::Green;
+    FastLED.show();
+    LEDaddress++;
+    if (LEDaddress == NUM_LEDS)
+    {
+      LEDaddress = 0;
+    }
+    delay(getTime(difficulty));
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH)
+    {
+     Playing = false;
+     CycleEnded = true;
+    }
+ }
+}
 
-  for(int i=0;i<NUM_LEDS;i++){
-    num=i;
-    leds[i]=CRGB::Red;
-    FastLED.show(); 
-    delay(100);
-    leds[i] = CRGB::Black;
+int getTime(int diff)
+{
+  int timeValue = 0;
+  switch (diff) 
+  {
+    case EASY:
+      timeValue = 100;
+      break;
+    case MEDIUM:
+      timeValue = 80;
+      break;
+    case HARD:
+      timeValue = 60;
+      break;
+    case ON_SPEED:
+      timeValue = 40;
+      break;
+    case SONIC_SPEED:
+      timeValue = 30;
+      break;
+    case ROCKET_SPEED:
+      timeValue = 20;
+      break;
+    case LIGHT_SPEED:
+      timeValue = 13;
+      break;
+    case MISSION_IMPOSSIBLE:
+      timeValue = 7;
+  }
+  return timeValue;
+}
+
+void increaseDifficulty() 
+{
+  if (difficulty != MISSION_IMPOSSIBLE && wonThisRound) 
+  {
+    difficulty++;
   }
 }
 
-void interruptBottone(){
-  if(num==25){
-    static uint8_t hue = 0;
-    for(int i = 0; i < NUM_LEDS; i++) {
-    // Set the i'th led to red 
+void flash()
+{ 
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
+  delay(500);
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show();
+  delay(500);
+}
+
+void fadeall() 
+{ 
+  for(int i = 0; i < NUM_LEDS; i++) 
+  {
+    leds[i].nscale8(250); 
+  } 
+}
+
+void cylon() 
+{ 
+  static uint8_t hue = 0;
+  Serial.print("x");
+  for(int i = 0; i < NUM_LEDS; i++) 
+  {
     leds[i] = CHSV(hue++, 255, 255);
-    // Show the leds
     FastLED.show(); 
-    // now that we've shown the leds, reset the i'th led to black
-    // leds[i] = CRGB::Black;
+    //leds[i] = CRGB::Black;
     fadeall();
-    // Wait a little bit before we loop around and do it again
     delay(10);
   }
+  Serial.print("x");
+
+  for(int i = (NUM_LEDS)-1; i >= 0; i--) 
+  {
+    leds[i] = CHSV(hue++, 255, 255);
+    FastLED.show();
+    //leds[i] = CRGB::Black;
+    fadeall();
+    delay(10);
   }
 }
